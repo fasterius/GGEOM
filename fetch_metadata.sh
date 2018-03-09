e70ffde1b506fb5a86f62ba83f9c3c1d0351bea2
Erik Fasterius <erikfas@kth.se>
2018-03-09 11:23:51 +0100

    Add more progress messages

--- fetch_metadata.sh
+++ fetch_metadata.sh
@@ -3,10 +3,6 @@
 # Wrapper script for downloading and parsing GEO/SRA metadata for a single GSE
 # series or a list of series (in a file), provided as first argument
 
-# Set bash strict mode
-set -euo pipefail
-IFS=$'\n\t'
-
 # Check if an argument exists
 if [ -z ${1+x} ]; then
     echo "ERROR: missing input parameters; aborting."
@@ -36,10 +32,11 @@ esearch -db sra -query SRP066982 \
         > sra_metadata.txt
 
 # Get runinfo for each SRP ID in file
-SRPCOL=$(head -1 geo_metadata.txt | tr '\t' '\n' | nl | grep "SRP" | cut -f 1)
+SRPCOL=$(head -1 geo_metadata.txt | xargs -n 1 | nl | grep "SRP" | cut -f 1)
 for SRP in $(tail -n +2 geo_metadata.txt | cut -f $SRPCOL | sort | uniq); do
 
     # Query SRA and add runinfo to file
+    echo "Fetching sequencing data for $SRP ..."
     esearch -db sra -query "$SRP" < /dev/null \
         | efetch -format runinfo \
         | tr ',' '\t' \
@@ -52,7 +49,7 @@ done
 # Merge metadata --------------------------------------------------------------
 
 # Merge the two metadata files and order columns
-echo "Merging metadata ..."
+echo "Merging GEO and SRA metadata ..."
 merge_metadata.R
 
 # Remove temporary files
